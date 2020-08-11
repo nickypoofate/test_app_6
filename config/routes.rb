3d958ed04ee698acaefed057b4e7e03ae2582312
nickypoofate <nicklaus.d.faith.esquire@gmail.com>
2020-08-11 10:11:43 -0500

    added about page, working with emmett tags!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#home' #found in app>controllers...the _controller is implied
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  #found in app>controllers...the _controller is implied
   # the pound plus name is an action --
   # needs to be defined in application controller.rb
+
 end
